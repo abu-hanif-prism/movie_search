fcd173a9d0cc320f2c20a3d5652b64ba4a0bc8ff
MD Abu Hanif <hanif.ov54@gmail.com>
2026-09-18 08:00:00 +0000

    Connect home and movie pages with hash navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,18 @@
-import React from 'react';
-export default function App() { return <h1>MovieExplorer</h1>; }
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+
+export default function App() {
+  const [page, setPage] = useState(window.location.hash === '#/movies' ? 'movies' : 'home');
+  useEffect(() => {
+    function changePage() {
+      setPage(window.location.hash === '#/movies' ? 'movies' : 'home');
+      window.scrollTo(0, 0);
+    }
+    window.addEventListener('hashchange', changePage);
+    return () => window.removeEventListener('hashchange', changePage);
+  }, []);
+  return <div className="app"><Navbar page={page} /><main>{page === 'movies' ? <Movies /> : <Home />}</main><Footer /></div>;
+}
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function Movies() { return <div className="container listing"><h1>Browse movies & shows</h1></div>; }
